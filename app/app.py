7717004b45a9d3854f84d1d88d88a499eddca07a
Marcin Kaminski <marcin.kaminski@dreamlab.pl>
2020-01-08 11:24:03 +0100

    ✅ Added exemplary test

--- app/app.py
+++ app/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from api import init_api
 from config import DEBUG_MODE
 
-APP = Flask(__name__)
-
-init_api(app=APP)
+def main():
+    app = Flask(__name__)
+    init_api(app=app)
+    app.run(debug=DEBUG_MODE)
 
 if __name__ == "__main__":
-    APP.run(debug=DEBUG_MODE)
+    main()
