59028ac1bec7b75bde7875cf56661e4bf345f682
Marcin Kaminski <marcin.kaminski@dreamlab.pl>
2020-01-11 20:07:53 +0100

    ✨ Added stars

--- rate_your_mate/mocks.py
+++ rate_your_mate/mocks.py
@@ -50,14 +50,24 @@ CATEGORIES = {
         "name": "javascript",
         "id": "c2bb7366-e76a-42bf-a844-8596d81e158e",
         "stars": 20,
-        "data": {
+        "dates": {
             "2020-01-10": {
                 "stars": 8,
-                "users": [{"id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc", "stars": 8}],
+                "users": {
+                    "8fc6734e-4765-48d9-88c2-fb9da65d52bc": {
+                        "id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc",
+                        "stars": 8,
+                    }
+                },
             },
             "2020-01-11": {
                 "stars": 12,
-                "users": [{"id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc", "stars": 12}],
+                "users": {
+                    "8fc6734e-4765-48d9-88c2-fb9da65d52bc": {
+                        "id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc",
+                        "stars": 12,
+                    }
+                },
             },
         },
     },
@@ -65,14 +75,24 @@ CATEGORIES = {
         "name": "python",
         "id": "b3edb784-fa7f-4ea7-a400-b3cdc7e81651",
         "stars": 10,
-        "data": {
+        "dates": {
             "2020-01-10": {
                 "stars": 2,
-                "users": [{"id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc", "stars": 2}],
+                "users": {
+                    "8fc6734e-4765-48d9-88c2-fb9da65d52bc": {
+                        "id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc",
+                        "stars": 2,
+                    }
+                },
             },
             "2020-01-11": {
                 "stars": 8,
-                "users": [{"id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc", "stars": 8}],
+                "users": {
+                    "8fc6734e-4765-48d9-88c2-fb9da65d52bc": {
+                        "id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc",
+                        "stars": 8,
+                    }
+                },
             },
         },
     },
--- rate_your_mate/stars.py
+++ rate_your_mate/stars.py
@@ -1,19 +1,45 @@
 from flask_restful import Resource
 from rate_your_mate.mocks import USERS, CATEGORIES
+from datetime import date
+
+ADD_STAR_VALUE = 1
 
 def add_star_to_user(user_id: str, category_id: str):
-    user = USERS[user_id]
+    USERS[user_id]["stars"] += ADD_STAR_VALUE
 
-    user["stars"] += 1
-
-    if category_id in user["categories"]:
-        user["categories"][category_id]["stars"] += 1
+    if category_id in USERS[user_id]["categories"]:
+        USERS[user_id]["categories"][category_id]["stars"] += ADD_STAR_VALUE
     else:
-        pass
+        USERS[user_id]["categories"][category_id] = {
+            "id": category_id,
+            "name": CATEGORIES[category_id]["name"]
+            "stars": ADD_STAR_VALUE
+        }
 
 
-def add_star_to_category():
-    pass
+def add_star_to_category(user_id: str, category_id: str):
+    CATEGORIES[category_id]["stars"] += ADD_STAR_VALUE
+
+    today = date.today()
+
+    if today in CATEGORIES[category_id]["dates"]:
+        CATEGORIES[category_id]["dates"][today]["stars"] += ADD_STAR_VALUE
+
+        if user_id in CATEGORIES[category_id]["dates"][today]["users"]:
+            CATEGORIES[category_id]["dates"][today]["users"][user_id]["stars"] += ADD_STAR_VALUE
+        else:
+            CATEGORIES[category_id]["dates"][today]["users"][user_id] = {"id": user_id, "stars": ADD_STAR_VALUE}
+
+    else:
+        CATEGORIES[category_id]["dates"][today] = {
+            "stars": ADD_STAR_VALUE,
+            "users": {
+                f'{user_id}':{
+                    "id": user_id,
+                    "stars": ADD_STAR_VALUE
+                }
+            }
+        }
 
 class Stars(Resource):
     def post(self) -> str:
@@ -24,3 +50,6 @@ class Stars(Resource):
 
         user_id = stared.get("user", {})["id"]
         category_id = stared.get("category", {})["id"]
+
+        add_star_to_user(user_id=user_id, category_id=category_id)
+        add_star_to_category(user_id=user_id, category_id=category_id)
