694849be04ec06e266b2495c88be41cf4ff6a5ff
Marcin Kaminski <marcin.kaminski@dreamlab.pl>
2020-01-11 18:45:59 +0100

    ✨ Added exemplary user and uuid generator

--- rate_your_mate/users.py
+++ rate_your_mate/users.py
@@ -5,6 +5,7 @@ USERS = [
         "name": "Marcin Kamiński",
         "id": "8fc6734e-4765-48d9-88c2-fb9da65d52bc",
         "github": "https://github.com/marcinxkaminski",
+        "linkedin": "https://www.linkedin.com/in/marcinxkaminski/",
         "avatar": "https://avatars2.githubusercontent.com/u/26044111?s=460&v=4",
         "room": "101",
         "city": "Cracow",
@@ -29,7 +30,30 @@ USERS = [
                 "value": 10000,
             }
         },
-    }
+    },
+    {
+        "name": "Adam Kowalski",
+        "id": "74f9875d-400e-4508-b70a-bcb090d338fd",
+        "github": "https://github.com/abc",
+        "linkedin": "https://www.linkedin.com/in/abc",
+        "avatar": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftranquilmonkey.com%2Fwp-content%2Fuploads%2F2016%2F05%2Fhuman-documentary.png&f=1&nofb=1",
+        "room": "202",
+        "city": "Cracow",
+        "position": "Back-End Engineer",
+        "categories": {
+            "c2bb7366-e76a-42bf-a844-8596d81e158e": {
+                "name": "javascript",
+                "id": "c2bb7366-e76a-42bf-a844-8596d81e158e",
+                "stars": 0,
+            },
+            "b3edb784-fa7f-4ea7-a400-b3cdc7e81651": {
+                "name": "python",
+                "id": "b3edb784-fa7f-4ea7-a400-b3cdc7e81651",
+                "stars": 0,
+            },
+        },
+        "badges": {},
+    },
 ]
 
 
@@ -41,6 +65,11 @@ class Users(Resource):
         return USERS
 
     def post(self) -> str:
+        """
+        Adds new user
+        """
+        json_data = request.get_json(force=True)
+
         return "POST"
 
     def put(self) -> str:
---
+++ rate_your_mate/uuid.py
@@ -0,0 +1,5 @@
+from uuid import uuid4
+
+
+def create():
+    return uuid4()
